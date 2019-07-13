c9af1adc9a61c57321694bec39e6b88c2bfd63f0
pritesh99 <52821885+pritesh99@users.noreply.github.com>
2019-07-13 15:48:50 +0530

    my first project

---
+++ friends.java
@@ -0,0 +1,10 @@
+class friends
+{
+public static void main (String args[])
+{
+System.out.println(args[1]+" "+args[2]);
+System.out.println(args[3]+" "+args[4]);
+System.out.println(args[5]+" "+args[6]);
+System.out.println(args[7]+" "+args[8]);
+}
+}
